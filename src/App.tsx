32b70bff0ad4f746aab462d3d8a220e5a8ccfa64
Breyanna Vass <breyannavass@gmail.com>
2019-12-09 22:28:24 -0500

    created a function to check when the game is over

--- src/App.tsx
+++ src/App.tsx
@@ -46,6 +46,32 @@ class App extends React.Component<{}, IState> {
     return -1;
   };
 
+  public checkIfGameIsOver = (board: Player[]) => {
+    let winCheckSlices = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]]
+    ];
+
+    for (let i = 0; i < winCheckSlices.length; i++) {
+      const winResult = this.sliceWinCheck(winCheckSlices[i]);
+      if (winResult !== -1) {
+        return winResult;
+      }
+    }
+
+    if (board.some(cell => cell === Player.None)) {
+      return ONGOING_GAME;
+    }
+
+    return -1;
+  };
+
   public render() {
     return (
       <div className="App">
